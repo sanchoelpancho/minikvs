adaa82e48e7c725f72048b3284df02f63c53822c
João Sanches <jota.sanches@sapo.pt>
2026-07-16 15:38:22 +0100

    add PUT

--- src/kvs_handler.erl
+++ src/kvs_handler.erl
@@ -8,8 +8,8 @@ init(Req0, State) ->
     Req1 = case Method of
         <<"GET">> -> 
             Reply = case kvs_client:get(Key) of
-                {ok, {Key, Value}}      -> {200, #{status => <<"found">>, key => Key, value => Value}};
-                {error, key_not_found}  -> {404, #{status => <<"key not found">>}}
+                {ok, {GetKey, GetData}}           ->      {200, #{status => <<"found">>, key => GetKey, data => GetData}};
+                {error, key_not_found}      ->      {404, #{status => <<"key not found">>}}
             end,
             {Status, Map} = Reply,
             Body = json:encode(Map),
@@ -17,6 +17,19 @@ init(Req0, State) ->
                 #{<<"content-type">> => <<"application/json">>},
                 Body,
                 Req0);
+        <<"PUT">> ->
+            {ok, ReqBody, Req0a} = cowboy_req:read_body(Req0),
+            KeyValuePair = json:decode(ReqBody),
+            Reply = case kvs_client:set(maps:get(<<"key">>, KeyValuePair), maps:get(<<"data">>, KeyValuePair)) of
+                {ok, {SetKey, SetData}, added}        ->    {200, #{status => <<"added">>, key => SetKey, data => SetData}};
+                {_}                                   ->    {400, #{status => <<"error">>}}
+            end,
+            {Status, Map} = Reply,
+            Body = json:encode(Map),
+            cowboy_req:reply(Status,
+                #{<<"content-type">> => <<"application/json">>},
+                Body,
+                Req0a);
         _ ->
             cowboy_req:reply(405, #{}, <<>>, Req0)
     end,
