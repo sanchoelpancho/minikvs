9eb45416c10b2798e6674a3eb396eb30a4188add
João Sanches <jota.sanches@sapo.pt>
2026-07-16 15:59:43 +0100

    fix bug

--- src/kvs_handler.erl
+++ src/kvs_handler.erl
@@ -23,7 +23,7 @@ init(Req0, State) ->
             {ok, ReqBody, Req0a} = cowboy_req:read_body(Req0),
             KeyValuePair = json:decode(ReqBody),
             Reply = case kvs_client:set(maps:get(<<"key">>, KeyValuePair), maps:get(<<"data">>, KeyValuePair)) of
-                {ok, {SetKey, SetData}, added}        ->    {200, #{status => <<"added">>, key => SetKey, data => SetData}};
+                {ok, {SetKey, SetData}, added}        ->    {200, #{status => <<"added">>, key => SetKey, data => SetData}}
             end,
             {Status, Map} = Reply,
             Body = json:encode(Map),
