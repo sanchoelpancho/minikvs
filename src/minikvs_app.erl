bda57ce9bac29ddd4ecb2437629ee45094c7843e
João Sanches <jota.sanches@sapo.pt>
2026-07-17 09:10:21 +0100

    Update minikvs_app.erl

--- src/minikvs_app.erl
+++ src/minikvs_app.erl
@@ -17,8 +17,7 @@ start(_StartType, _StartArgs) ->
     cowboy:start_clear(my_http_listener,
         [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
-    ),
-    {SupPid}.
+    ).
 
 stop(_State) ->
     kvs_db:stop_db(),
